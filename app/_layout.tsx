8e4dc37e63734e59007e3e7f1c871bd0d0edf33a
KOKI-programming <kokimatuba@gmail.com>
2026-05-10 23:35:00 +0900

    Add useFrameworkReady to fix Bolt preview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useFrameworkReady } from "@/hooks/useFrameworkReady";
 
 void SplashScreen.preventAutoHideAsync();
 
@@ -34,6 +35,8 @@ function RootLayoutNav() {
 }
 
 export default function RootLayout() {
+  useFrameworkReady();
+
   useEffect(() => {
     void SplashScreen.hideAsync();
   }, []);
